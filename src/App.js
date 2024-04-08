bd095bb6e1fbd75fd3a037c968ce5c083a55e965
SaskiaVdZ <143086816+SaskiaVdZ@users.noreply.github.com>
2024-04-08 13:53:54 +0200

    display advice

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 
 import './App.css';
+import { useState } from 'react';
+
 
 function App() {
-
+const [advice, setAdvice]= useState("");
+//whever we need anything to change in the interface we need state
 async function getAdvice() {
 const res = await fetch ('https://api.adviceslip.com/advice');
 const data = await res.json();
-console.log(data);
+//fetches data from an external source and stores it in a variable
+setAdvice(data.slip.advice);
+
 //async and await make promises easier to write
 //async makes a function return a Promise
 //await makes a function wait for a Promise
@@ -15,7 +20,7 @@ console.log(data);
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello world!</h1>
+        <h1>{advice}</h1>
         <button onClick={getAdvice}>Get advice</button>
       </header>
     </div>
