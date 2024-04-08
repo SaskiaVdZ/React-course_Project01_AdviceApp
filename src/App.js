684e27c04bb84d53c852e9cd1a38f051e0cde92e
SaskiaVdZ <143086816+SaskiaVdZ@users.noreply.github.com>
2024-04-08 14:07:32 +0200

    add counter

--- src/App.js
+++ src/App.js
@@ -6,11 +6,15 @@ import { useState } from 'react';
 function App() {
 const [advice, setAdvice]= useState("");
 //whever we need anything to change in the interface we need state
+const [count,setCount]= useState(0);
+
 async function getAdvice() {
 const res = await fetch ('https://api.adviceslip.com/advice');
 const data = await res.json();
 //fetches data from an external source and stores it in a variable
 setAdvice(data.slip.advice);
+setCount (c => c + 1);
+//takes current count and adds 1 which will be the new count
 
 //async and await make promises easier to write
 //async makes a function return a Promise
@@ -22,6 +26,7 @@ setAdvice(data.slip.advice);
       <header className="App-header">
         <h1>{advice}</h1>
         <button onClick={getAdvice}>Get advice</button>
+        <p>You have read <strong>{count}</strong> pieces of advice</p>
       </header>
     </div>
   );
