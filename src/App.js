6c24dd4539fe265fa494a8ff2745b997330d0069
SaskiaVdZ <143086816+SaskiaVdZ@users.noreply.github.com>
2024-04-08 13:35:10 +0200

    add button and function getAdvice

--- src/App.js
+++ src/App.js
@@ -2,10 +2,18 @@
 import './App.css';
 
 function App() {
+
+async function getAdvice() {
+const res = await fetch ('https://api.adviceslip.com/advice');
+const data = await res.json();
+console.log(data);
+}
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Hello world!</h1>
+        <button onClick={getAdvice}>Get advice</button>
       </header>
     </div>
   );
